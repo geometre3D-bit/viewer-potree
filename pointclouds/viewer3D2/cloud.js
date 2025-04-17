b9f2fdc8f63aa2c1ff7576b102bccc4784370fd8
Hamza BOUFROUR <boufrourhamza20@gmail.com>
2025-04-17 08:38:23 +0200

    Première version du viewer Potree

---
+++ pointclouds/viewer3D2/cloud.js
@@ -0,0 +1,91 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 11697177,
+    "boundingBox": {
+        "lx": 572601.875,
+        "ly": 6724000.0,
+        "lz": 110.02,
+        "ux": 573440.185,
+        "uy": 6724838.3100000009,
+        "uz": 948.3300000000559
+    },
+    "tightBoundingBox": {
+        "lx": 572601.875,
+        "ly": 6724000.0,
+        "lz": 110.02,
+        "ux": 573440.185,
+        "uy": 6724682.5,
+        "uz": 154.20000000000003
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 7.259977340698242,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
